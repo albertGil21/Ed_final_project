a68e33e571ff5e105ed1fa314b6bc9379626e639
albertGil21 <alberto.gil@unmsm.edu.pe>
2024-12-09 21:57:27 -0500

    okay

---
+++ src/main/java/controlador/ed_proyecto_final/Ed_Proyecto_final.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package controlador.ed_proyecto_final;
+
+/**
+ *
+ * @author ALGS
+ */
+public class Ed_Proyecto_final {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
